793406266ec0283e150e2f1f69189bc5dafc223f
kapyanammu1 <xtianized1@gmail.com>
2023-11-09 04:27:02 +0800

    ddf

--- BrgySystem/settings.py
+++ BrgySystem/settings.py
@@ -148,11 +148,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS  = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
-<<<<<<< HEAD
 MEDIA_ROOT = BASE_DIR / 'media'
-=======
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
->>>>>>> 2af0412028486c72f5ede17644ce986d6a686100
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
